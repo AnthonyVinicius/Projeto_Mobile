498fd28aaa137fff3da711fb658bd190efb70998
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2025-11-23 19:22:47 -0300

    feat: Set up Retrofit for CoinGecko API integration
    Adds the Retrofit dependency and creates the necessary classes to fetch coin data from the CoinGecko API.
    
    This includes:
    - `Coin` data class for the domain model.
    - `CoinDTO` for the API response.
    - `CoinGeckoApi` interface with a `getCoins` endpoint.
    - `RetrofitInstance` singleton to provide the API service.
    - `CoinRepository` and `ICoinRepository` for the repository pattern.

---
+++ Alvarium/app/src/main/java/br/edu/ifpe/alvarium/data/remote/dto/CoinDTO.kt
@@ -0,0 +1,7 @@
+package br.edu.ifpe.alvarium.data.remote.dto
+
+data class CoinDTO(
+    val id: String,
+    val symbol: String,
+    val name: String
+)
---
+++ Alvarium/app/src/main/java/br/edu/ifpe/alvarium/domain/model/Coin.kt
@@ -0,0 +1,7 @@
+package br.edu.ifpe.alvarium.domain.model
+
+data class Coin(
+    val id: String,
+    val symbol: String,
+    val name: String
+)
